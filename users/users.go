a7d47269c3678292ec27cd28e9bc9a97a67b4a43
edmond fagbuyi <edmondfagbuyi@gmail.com>
2023-06-07 01:13:28 +0100

    add registeration function

--- users/users.go
+++ users/users.go
@@ -45,31 +45,86 @@ func prepareResponse(user *models.User, account []models.ResponseAccount) map[st
 
 func Login(username string, pass string) map[string]interface{} {
 
-	//Connect db
-	db := helpers.ConnectDB()
-	user := &models.User{}
-	if db.Where("username = ?", username).First(&user).RecordNotFound() {
-		return map[string]interface{}{"message": "User not found"}
+	//Before connect to db validate credentials
+	valid := helpers.Validation(
+		[]models.Validation{
+			{Value: username, Valid: "username"},
+			{Value: pass, Valid: "password"},
+		})
+
+	if valid {
+		//Connect db
+		db := helpers.ConnectDB()
+		user := &models.User{}
+		if db.Where("username = ?", username).First(&user).RecordNotFound() {
+			return map[string]interface{}{"message": "User not found"}
+		}
+
+		//Verify pasword
+		passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
+
+		if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
+			return map[string]interface{}{"message": "Wrong password"}
+		}
+
+		//Find account for the user
+		account := []models.ResponseAccount{}
+		db.Table("account").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&account)
+
+		defer db.Close()
+
+		var response = prepareResponse(user, account)
+
+		return response
+
+	} else {
+		return map[string]interface{}{"message": "Not valid credentials"}
 	}
 
-	//Verify pasword
-	passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
-
-	if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
-		return map[string]interface{}{"message": "Wrong password"}
-	}
-
-	//Find account for the user
-	account := []models.ResponseAccount{}
-	db.Table("account").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&account)
-
-	defer db.Close()
-
-	var response = prepareResponse(user, account)
-
-	return response
 }
 
 func Register(username, email, pass string) map[string]interface{} {
+	valid := helpers.Validation(
+		[]models.Validation{
+			{Value: username, Valid: "username"},
+			{Value: email, Valid: "email"},
+			{Value: pass, Valid: "password"},
+		})
 
+	if valid {
+		db := helpers.ConnectDB()
+		generatePassword := helpers.HashAndSalt([]byte(pass))
+		user := models.User{
+			Username: username,
+			Email:    email,
+			Password: generatePassword,
+		}
+		db.Create(&user)
+
+		account := models.Account{
+			Type:    "Savings Account",
+			Name:    string(username + "'s" + " account"),
+			Balance: uint(0),
+			UserID:  user.ID,
+		}
+		db.Create(&account)
+
+		defer db.Close()
+
+		accounts := []models.ResponseAccount{}
+		respAccount := models.ResponseAccount{
+			ID:      account.ID,
+			Name:    account.Name,
+			Balance: int(account.Balance),
+		}
+		accounts = append(accounts, respAccount)
+
+		var response = prepareResponse(&user, accounts)
+
+		return response
+
+
+	} else {
+		return map[string]interface{}{"message": "Not valid credentials"}
+	}
 }
